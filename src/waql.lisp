630cd22d0b45108c9ae620fb0ff429ae46a57a48
Masayuki Takagi <kamonama@gmail.com>
2013-07-26 12:39:23 +0900

    first commit

--- src/waql.lisp
+++ src/waql.lisp
@@ -5,4 +5,114 @@
 
 (in-package :waql)
 
-;; blah blah blah.
+;;;
+;;; User
+;;;
+
+(defstruct (user (:constructor user (id)))
+  (id nil :type fixnum :read-only t))
+
+
+;;;
+;;; Action Event
+;;;
+
+(defstruct (action-event (:constructor action-event (id)))
+  (id nil :type fixnum :read-only t))
+
+
+;;;
+;;; Action
+;;;
+
+(defstruct (action (:constructor action (id)))
+  (id nil :type fixnum :read-only t))
+
+
+;;;
+;;; Conversion Event
+;;;
+
+
+;;;
+;;; Conversion
+;;;
+
+
+;;;
+;;; Tuple
+;;;
+
+(defstruct (tuple (:constructor %make-tuple)
+                  (:conc-name %tuple-))
+  (elements nil :type list :read-only t))
+
+(defun tuple (&rest args)
+  (%make-tuple :elements args))
+
+(defmacro with-tuple (vars tuple &body body)
+  `(destructuring-bind ,vars (%tuple-elements ,tuple)
+     ,@body))
+
+
+;;;
+;;; Relation
+;;;
+
+(defstruct (relation (:constructor empty-relation ())
+                     (:conc-name %relation-))
+  (body (make-hash-table :test #'equalp) :type hash-table :read-only t))
+
+(defun relation->list (relation)
+  ;; CAUTION: relation has no order naturally
+  (alexandria:hash-table-keys (%relation-body relation)))
+
+(defun relation-member (item relation)
+  (check-type item tuple)
+  (let ((body (%relation-body relation)))
+    (values (gethash item body))))
+
+(defun relation-count (relation)
+  (hash-table-count (%relation-body relation)))
+
+(defun relation-adjoin (item relation)
+  (check-type item tuple)
+  (symbol-macrolet ((body (%relation-body relation)))
+    (setf (gethash item body) t)
+    relation))
+
+
+;;;
+;;; Extending :ITERATE library for relation
+;;;
+
+;;; driver for relation
+
+(defun %relation-elt-tuple-elements (relation index)
+  (%tuple-elements (elt (relation->list relation) index)))
+
+(defun %relation-length (relation)
+  (length (relation->list relation)))
+
+(iterate:defclause-sequence in-relation nil
+  :access-fn '%relation-elt-tuple-elements
+  :size-fn '%relation-length
+  :sequence-type 'relation
+  :element-type 'tuple
+  :element-doc-string "Tuples of a relation")
+
+;;; gatherer for relation
+
+(defmacro collect-relation (expr)
+  `(reducing ,expr
+          by #'(lambda (r i)
+                 (relation-adjoin i r))
+          initial-value (empty-relation)))
+
+
+;;;
+;;; Querying
+;;;
+
+(defmacro query ()
+  nil)
--- t/test-waql.lisp
+++ t/test-waql.lisp
@@ -7,6 +7,123 @@
 
 (plan nil)
 
-;; blah blah blah.
+;;;
+;;; test User
+;;;
+
+;;; test USER constructor
+(ok (user 1))
+
+;;; test USER-ID accessor
+(is (user-id (user 1)) 1)
+
+
+;;;
+;;; test Action Event
+;;;
+
+;;; test ACTION-EVENT constructor
+(ok (action-event 1))
+
+;;; test ACTION-EVENT-ID accessor
+(is (action-event-id (action-event 1)) 1)
+
+
+;;;
+;;; test Action
+;;;
+
+;;; test ACTION constructor
+(ok (action 1))
+
+;;; test ACTION-ID accessor
+(is (action-id (action 1)) 1)
+
+
+;;;
+;;; test Conversion Event
+;;;
+
+
+;;;
+;;; test Conversion
+;;;
+
+
+;;;
+;;; test Tuple
+;;;
+
+;;; test TUPLE constructor
+(ok (tuple 1 2 3))
+
+;;; test WITH-TUPLE macro
+(with-tuple (x y z) (tuple 1 2 3)
+  (is x 1)
+  (is y 2)
+  (is z 3))
+
+;;; test TUPLE-P function
+(ok (tuple-p (tuple 1 2 3)))
+(ok (null (tuple-p 1)))
+
+;;; test EQUALP function for tuple
+(ok (equalp (tuple 1 2 3) (tuple 1 2 3)))
+(is (equalp (tuple 1 2 3) (tuple 4 5 6)) nil)
+(is (equalp (tuple 1 2 3) (tuple 1 2 3 4)) nil)
+(ok (equalp (tuple (user 1)) (tuple (user 1))))
+
+
+;;;
+;;; test Relation
+;;;
+
+;;; test EMPTY-RELATION constructor
+(ok (empty-relation))
+
+;;; test RELATION->LIST function
+(let ((relation (empty-relation)))
+  (relation-adjoin (tuple 1) relation)
+  (is (relation->list relation) (list (tuple 1)) :test #'equalp))
+
+;;; test RELATION-MEMBER function
+
+;;; test RELATION-COUNT function
+
+;;; test RELATION-ADJOIN function
+(let ((cl-test-more:*default-test-function* #'equalp)
+      (relation (empty-relation)))
+  ;; adjoin tuple to relation
+  (relation-adjoin (tuple (user 1)) relation)
+  (ok (relation-member (tuple (user 1)) relation))
+  ;; adjoin another tuple to relation
+  (relation-adjoin (tuple (user 2)) relation)
+  (ok (relation-member (tuple (user 1)) relation))
+  (ok (relation-member (tuple (user 2)) relation))
+  ;; adjoining tuple ever adjoined to relation makes nothing
+  (relation-adjoin (tuple (user 2)) relation)
+  (ok (relation-member (tuple (user 1)) relation))
+  (ok (relation-member (tuple (user 2)) relation))
+  (is (relation-count relation) 2))
+
+;;; error if try to adjoin other tuples having different attributes
+(let ((relation (empty-relation)))
+  (relation-adjoin (tuple 1) relation)
+  (is-error (relation-adjoin (tuple (user 1)) relation) simple-error))
+
+;;; test extension for :ITERATE library on relation
+(let ((relation (relation-adjoin (tuple 4 5 6)
+                  (relation-adjoin (tuple 1 2 3)
+                    (empty-relation)))))
+  (let ((relation2 (iter (for (x y z) in-relation relation)
+                         (collect-relation (tuple x y z)))))
+    (ok (relation-member (tuple 1 2 3) relation2))
+    (ok (relation-member (tuple 4 5 6) relation2))))
+
+
+;;;
+;;; test Querying
+;;;
+
 
 (finalize)
