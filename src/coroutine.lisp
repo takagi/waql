f117ff68eb4c9c0cd55ea5d3a4d54efe044b7125
Masayuki Takagi <kamonama@gmail.com>
2013-09-15 01:15:19 +0900

    modify to remove EVAL-WHEN forms which are not needed now

--- src/coroutine.lisp
+++ src/coroutine.lisp
@@ -18,59 +18,57 @@
        (defcor/arg name (car args) body)
        (defcor/no-arg name body)))
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun defcor/arg (name arg body)
-    (alexandria:with-gensyms (cont)
-      `(setf (get ',name 'make-coroutine)
-             #'(lambda ()
-                 (let (,cont)
-                   #'(lambda (,arg)
-                       (declare (ignorable ,arg))
-                       (if ,cont
-                         (funcall ,cont ,arg)
-                         (cl-cont:with-call/cc
-                           (macrolet ((yield (&rest args)
-                                        (let ((cc (gensym)))
-                                          `(setf ,',arg
-                                                 (cl-cont:let/cc ,cc
-                                                   (setf ,',cont ,cc)
-                                                   (values ,@args)))))
-                                      (coexit (&rest args)
-                                        `(cl-cont:let/cc _
-                                           (declare (ignorable _))
-                                           (setf ,',cont
-                                                 #'(lambda (_)
-                                                     (declare (ignorable _))
-                                                     (values)))
-                                           (values ,@args))))
-                             ,@body
-                             (coexit nil)))))))))))
+(defun defcor/arg (name arg body)
+  (alexandria:with-gensyms (cont)
+    `(setf (get ',name 'make-coroutine)
+           #'(lambda ()
+               (let (,cont)
+                 #'(lambda (,arg)
+                     (declare (ignorable ,arg))
+                     (if ,cont
+                       (funcall ,cont ,arg)
+                       (cl-cont:with-call/cc
+                         (macrolet ((yield (&rest args)
+                                      (let ((cc (gensym)))
+                                        `(setf ,',arg
+                                               (cl-cont:let/cc ,cc
+                                                 (setf ,',cont ,cc)
+                                                 (values ,@args)))))
+                                    (coexit (&rest args)
+                                      `(cl-cont:let/cc _
+                                         (declare (ignorable _))
+                                         (setf ,',cont
+                                               #'(lambda (_)
+                                                   (declare (ignorable _))
+                                                   (values)))
+                                         (values ,@args))))
+                           ,@body
+                           (coexit nil))))))))))
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun defcor/no-arg (name body)
-    (alexandria:with-gensyms (cont)
-      `(setf (get ',name 'make-coroutine)
-             #'(lambda ()
-                 (let (,cont)
-                   #'(lambda ()
-                       (if ,cont
-                         (funcall ,cont)
-                         (cl-cont:with-call/cc
-                           (macrolet ((yield (&rest args)
-                                        (let ((cc (gensym)))
-                                          `(cl-cont:let/cc ,cc
-                                             (setf ,',cont ,cc)
-                                             (values ,@args))))
-                                      (coexit (&rest args)
-                                        `(cl-cont:let/cc _
-                                           (declare (ignorable _))
-                                           (setf ,',cont
-                                                 #'(lambda (_)
-                                                     (declare (ignorable _))
-                                                     (values)))
+(defun defcor/no-arg (name body)
+  (alexandria:with-gensyms (cont)
+    `(setf (get ',name 'make-coroutine)
+           #'(lambda ()
+               (let (,cont)
+                 #'(lambda ()
+                     (if ,cont
+                       (funcall ,cont)
+                       (cl-cont:with-call/cc
+                         (macrolet ((yield (&rest args)
+                                      (let ((cc (gensym)))
+                                        `(cl-cont:let/cc ,cc
+                                           (setf ,',cont ,cc)
                                            (values ,@args))))
-                             ,@body
-                             (coexit nil)))))))))))
+                                    (coexit (&rest args)
+                                      `(cl-cont:let/cc _
+                                         (declare (ignorable _))
+                                         (setf ,',cont
+                                               #'(lambda (_)
+                                                   (declare (ignorable _))
+                                                   (values)))
+                                         (values ,@args))))
+                           ,@body
+                           (coexit nil))))))))))
 
 
 ;;;
